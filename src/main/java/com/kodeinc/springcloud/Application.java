27ebd35da7f4cca6f3c747bb4a22a614f9e43343
Muyinda ROgers <moverr@gmail.com>
2020-05-27 00:11:54 +0300

    Testing Port Change config serv er

--- src/main/java/com/kodeinc/springcloud/Application.java
+++ src/main/java/com/kodeinc/springcloud/Application.java
@@ -1,14 +1,16 @@
 package com.kodeinc.springcloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableDiscoveryClient
+//@Configuration
+//@EnableAutoConfiguration
+//@EnableDiscoveryClient
+//@EnableConfigServer
+
+
+@SpringBootApplication
 @EnableConfigServer
 public class Application {
 
